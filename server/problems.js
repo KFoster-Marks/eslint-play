f2f2363888cece83178b9991a1baa682b02cb936
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2018-07-07 06:29:50 -0600

    Add problematic code for linting

---
+++ server/problems.js
@@ -0,0 +1,5 @@
+var problems = ['consoles', 'double quotes', 'function keywords'];
+
+for (i = 0; i < problems.length; i++) {
+    console.log(problems[i])
+}
---
+++ server/server.js
@@ -0,0 +1,3 @@
+var myVar = 'This should be a let or const'
+
+console.log(myVar)
